ff6c1c61c53fb5db04c96b8afda0ae194bdd4d74
ginger <291165660@qq.com>
2022-03-30 17:45:38 +0800

    task_330

--- task_cnn_mlp/test01.py
+++ task_cnn_mlp/test01.py
@@ -18,11 +18,24 @@ class Net(nn.Module):
 
 if __name__ == '__main__':
     # net = Net()
-    # demo = torch.randn(1, 3, 7, 7)
+    demo = torch.randn(1, 6, 3, 3)
     # print(net.forward(demo).shape)
-    print(models.resnet50())
-    demo = torch.randn(480)
+    # print(models.resnet50())
+    # demo = torch.randn(480)
     # print(demo.shape)
     # # 张量的条件筛选，大于0.5的值会转为True，再转为浮点就变成了1.
     # print((demo > 0.5).float())
     # print((demo[:, None]).shape)
+
+    # 深度可分离卷积
+    layer = nn.Conv2d(6, 6, 3, 1, groups=6)
+    out = layer(demo)
+    print(out.shape)
+    print(out)
+    # 使用reshape操作进行通道混洗
+    out_v1 = out.reshape(1,2,3,1,1)
+    # print(out_v1)
+    out_v2 = out_v1.permute(0,2,1,3,4)
+    # print(out_v2.shape)
+    result = out_v2.reshape(1,6,1,1)
+    print(result)
