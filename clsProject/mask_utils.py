6fcfa1a6310a1f9dad396777786664ce47c02ff7
ginger <291165660@qq.com>
2022-05-06 18:02:28 +0800

    0506

--- clsProject/mask_utils.py
+++ clsProject/mask_utils.py
@@ -5,10 +5,11 @@ with open(mask_path, 'r') as m:
     for line in m:
         # print(line.split("_"))
         for c in line.split("_"):
-            if len(c) > 1:
+            if len(c) > 4:
                 # print(c)
                 str_list.append("_" + c)
         str_list.append("\n")
 with open(save_path, 'a') as s:
     # print(str_list)
     s.write("".join(str_list))
+    print("save success!")
