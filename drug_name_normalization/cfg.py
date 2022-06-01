210474e1cbd6663083c82c098b256ec14c131add
ginger <291165660@qq.com>
2022-06-01 18:00:11 +0800

    0601

---
+++ drug_name_normalization/cfg.py
@@ -0,0 +1,4 @@
+file_path = r"G:\liewei\source\word_vec\atc数据.xlsx"
+save_path = r"G:\liewei\source\word_vec\atc数据.txt"
+embeddings_filepath = r"G:\liewei\source\word_vec\wikicw\sgns.wiki.char"
+word2vec_path = r"G:\liewei\source\word_vec\wikicw\word2vec.npy"
