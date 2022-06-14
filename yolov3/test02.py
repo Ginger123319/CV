d5871dc0ae54cf1d3081aff83e68c215a8cbb854
ginger <291165660@qq.com>
2022-06-14 18:01:39 +0800

    0614

--- yolov3/test02.py
+++ yolov3/test02.py
@@ -1,10 +1,10 @@
 import torch
 
-input = torch.arange(180).reshape(1,2,2,3,15)
+input = torch.arange(180).reshape(1, 2, 2, 3, 15)
 
 mask = input[..., 0] > 6
 print(mask)
-print("mask:===>",mask.shape)
+print("mask:===>", mask.shape)
 idxs = mask.nonzero()
 
 print(idxs)
