e394d09fd650e0ed05d6fdc69a7427895ec9c421
ginger <291165660@qq.com>
2022-06-24 15:27:22 +0800

    0624

---
+++ yolov3_keyboard_mouse/dataset.py
@@ -0,0 +1,108 @@
+import torch
+from torch.utils.data import Dataset
+from utils import pic_resize
+import torchvision
+import numpy as np
+import cfg
+import os
+
+from PIL import Image
+import math
+
+LABEL_FILE_PATH = "data/label.txt"
+IMG_BASE_DIR = "data/img_song"
+
+transforms = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor()
+])
+
+
+def one_hot(cls_num, v):
+    b = np.zeros(cls_num)
+    b[v] = 1.
+    return b
+
+
+class MyDataset(Dataset):
+
+    def __init__(self):
+        with open(LABEL_FILE_PATH) as f:
+            self.dataset = f.readlines()
+            # print(self.dataset)
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        # 用字典存储三个尺度的标签13，26，52
+        labels = {}
+        # 取出字符串列表中的一条数据
+        line = self.dataset[index]
+        # 将这个字符串按空格切分为字符列表，会忽略掉\n
+        strs = line.split()
+        # print(strs)
+        # exit()
+        # _img_data = Image.open(os.path.join(IMG_BASE_DIR, strs[0]))
+        # print(_img_data.size)
+
+        # _img_data = _img_data.resize((416, 416))  # 此处要等比缩放
+        _img_data, ratio = pic_resize(os.path.join(IMG_BASE_DIR, strs[0]))
+        # print("ratio:", ratio)
+        img_data = transforms(_img_data)
+        # print(img_data.shape)
+        # exit()
+        _boxes = np.array([float(x) for x in strs[1:]])
+        # _boxes = np.array(list(map(float, strs[1:])))
+        boxes = np.split(_boxes, len(_boxes) // 5)
+        print(len(boxes))
+        # 缩放标签,是否有更好的方式或者更加简洁的写法
+        for i in range(len(boxes)):
+            boxes[i][1:] = boxes[i][1:] * ratio
+
+        index = 0
+        for feature_size, anchors in cfg.ANCHORS_GROUP.items():
+            # 创建13尺度的标签，此处labels是个字典
+            # 键值对（13：标签张量）
+            labels[feature_size] = np.zeros(shape=(feature_size, feature_size, 3, 5 + cfg.CLASS_NUM))
+            # print(labels[feature_size].shape)
+            # exit()
+            for box in boxes:
+                cls, cx, cy, w, h = box
+                # 计算偏移量，以及中心点在标签上的索引位置
+                # math.modf先返回小数部分，再返回整数部分
+                # feature_size / cfg.IMG_WIDTH=(13,26,52)/416
+                cx_offset, cx_index = math.modf(cx * feature_size / cfg.IMG_WIDTH)
+                cy_offset, cy_index = math.modf(cy * feature_size / cfg.IMG_WIDTH)
+                # print(cx_index,cx_offset)
+                # exit()
+
+                # 取出三个尺寸的建议框
+                for i, anchor in enumerate(anchors):
+                    # 取出每个（i：0，1，2）建议框的面积
+                    anchor_area = cfg.ANCHORS_GROUP_AREA[feature_size][i]
+                    # 计算宽高的偏移量
+                    p_w, p_h = w / anchor[0], h / anchor[1]
+                    # 计算目标框和建议框的IOU
+                    p_area = w * h
+                    iou = min(p_area, anchor_area) / max(p_area, anchor_area)
+                    index += 1
+                    # 向目标中心点对应到标签上的索引位置填充一个IOU，四个偏移量，以及分类标签
+                    # 标签的形状是HWC，所以cy_index在前面
+                    # i表示向第几个框中填写这六个标签值
+                    # np.log(p_w)，因为宽高的偏移量只能是正数，取对数后，标签就有正有负，更利于模型训练
+                    labels[feature_size][int(cy_index), int(cx_index), i] = np.array(
+                        [iou, cx_offset, cy_offset, np.log(p_w), np.log(p_h), int(cls)])
+        return labels[13], labels[26], labels[52], img_data
+
+
+if __name__ == '__main__':
+    data = MyDataset()
+    # print(data[1][0])
+    # print(data[0][0][..., 0].shape)
+    for i in range(14):
+        print(data[i][0].shape)
+    # 打印所有的IOU
+    # print(data[2][0][..., -1])
+    # print(data[0][1].shape)
+    # print(data[0][2].shape)
+    # print(data[0][3].shape)
---
+++ yolov3_keyboard_mouse/tool.py
@@ -0,0 +1,58 @@
+import numpy as np
+import torch
+
+
+def ious(box, boxes, isMin=False):  # 定义iou函数
+    box_area = (box[3] - box[1]) * (box[4] - box[2])  # 计算置信度最大框的面积
+    area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 4] - boxes[:, 2])  # 计算其他所有框的面积
+    xx1 = torch.max(box[1], boxes[:, 1])  # 计算交集左上角x的坐标其他同理
+    yy1 = torch.max(box[2], boxes[:, 2])
+    xx2 = torch.min(box[3], boxes[:, 3])
+    yy2 = torch.min(box[4], boxes[:, 4])
+
+    # w = torch.max(0, xx2 - xx1)
+    # h = torch.max(0, yy2 - yy1)#获取最大值也可以用下面这种方法
+    w = torch.clamp(xx2 - xx1, min=0)  # 获取最大值
+    h = torch.clamp(yy2 - yy1, min=0)
+
+    inter = w * h  # 计算交集面积
+
+    if isMin:  # 用于判断是交集/并集，还是交集/最小面积（用于去掉同一个目标大框套小框中的小框）
+
+        ovr = inter / torch.min(box_area, area)
+    else:
+        ovr = inter / (box_area + area - inter)
+    # print("ovr is:", ovr)
+    return ovr
+
+
+def nms(boxes, thresh=0.45, isMin=False):  # 定义nms函数并传3个参数，分别是框，置信度阀值，是否最小面积
+
+    if boxes.shape[0] == 0:  # 获取框的个是看是否为0，为0没框就返回一个空的数组防止代码报错
+        return np.array([])
+
+    _boxes = boxes[(-boxes[:, 0]).argsort()]  # 对框进行排序按置信度从大到小的顺序
+    r_boxes = []  # 定义一个空的列表用来装合格的框
+
+    while _boxes.shape[0] > 1:
+        a_box = _boxes[0]  # 取出第一个（置信度最大的框）框作为目标框与其他框做iou
+        b_boxes = _boxes[1:]  # 取出剩下的所有框
+
+        r_boxes.append(a_box)  # 将第一个框添加到列表
+
+        index = np.where(ious(a_box, b_boxes, isMin) < thresh)  # 对框做iou将满足iou阀值条件的框留下并返回其索引
+        _boxes = b_boxes[index]  # 根据索引取框并赋值给_boxes，使其覆盖原来的_boxes
+    if _boxes.shape[0] > 0:  # 判断是否剩下最后一个框
+        r_boxes.append(_boxes[0])  # 将最后一个框，说明这是不同物体，并将其放进列表
+
+    return torch.stack(r_boxes)
+
+
+if __name__ == '__main__':
+    # a = np.array([1,1,11,11])
+    # bs = np.array([[1,1,10,10],[11,11,20,20]])
+    # print(iou(a,bs))
+
+    bs = torch.tensor([[1, 1, 10, 10, 12, 8], [5, 1, 11, 5, 15, 9], [9, 8, 13, 9, 15, 3], [6, 11, 18, 17, 40, 2]])
+    # print(bs[:,3].argsort())
+    print(nms(bs))
